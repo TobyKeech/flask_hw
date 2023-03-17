43d62141c3ccd7f03c0b7518aeeb980b9d4ac7c6
Toby Keech <toby.keech18@gmail.com>
2023-03-17 09:42:39 +0000

    adding new book function

--- weekend/book_shop/controllers/controller.py
+++ weekend/book_shop/controllers/controller.py
@@ -15,3 +15,16 @@ def order(id):
     return render_template('order.html', 
                            title = 'CodeClan Libary', 
                            solo_book = book_list[int(id)])
+
+@app.route('/books', method = ['POST'])
+def add_new_book():
+    book_title = request.form['title']
+    book_author = request.form['author'] 
+    book_genre = request.form['genre']
+
+    new_book = Book(book_title, book_author, book_genre)
+
+    add_new_book(new_book)
+
+    return redirect ('/books')
+        
