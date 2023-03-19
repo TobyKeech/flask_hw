d20a938e605b7bad45bd3dfb7fcdbe4ecb0f5fda
Toby Keech <toby.keech18@gmail.com>
2023-03-19 18:14:56 +0000

    adding changes to completed extensions

--- weekend/book_shop/controllers/controller.py
+++ weekend/book_shop/controllers/controller.py
@@ -1,5 +1,4 @@
 from flask import *
-
 from app import app
 from models.book_list import *
 from models.book import *
@@ -10,55 +9,35 @@ def home():
     return render_template('base.html',
                            title = 'CodeClan Libary')
 
-
-@app.route('/books/<id>')
-def order(id):
-    return render_template('order.html', 
-                           title = 'CodeClan Libary', 
-                           solo_book = book_list[int(id)])
-
-@app.route('/books/new')
-def newbook():
-    return render_template ('newbook.html',
-                            title = 'CodeClan Libary')
-
 @app.route('/books')
-def index():
+def books_list():
     return render_template('index.html', 
                            title = 'CodeClan Libary', 
                            books_in_store = book_list)
 
+@app.route('/books/<index>')
+def book_show(index):
+    return render_template('order.html', 
+                           title = 'CodeClan Libary', 
+                           solo_book = book_list[int(index)])
+@app.route('/books/new')
+def book_new():
+    return render_template('new.html', 
+                           title = 'CodeClan Libary', 
+                           books_in_store = book_list)
 
-@app.route('/books', methods = ['POST'])
+@app.route('/books/new', methods = ['POST'])
 def add_book():
     book_title = request.form['title']
     book_author = request.form['author'] 
     book_genre = request.form['genre']
-
-    new_book = Book(book_title, book_author, book_genre)
-
+    new_book = Book(book_title, book_author, book_genre, True)
     add_new_book(new_book)
-
     return redirect ('/books')
 
 
-# @app.route('/books/list', methods = ['DELETE'])
-# def delete_book():
-#     book_title = request.form['title']
-#     book_author = request.form['author'] 
-#     book_genre = request.form['genre']
-
-#     book_to_delete = Book(book_title, book_author, book_genre)
-
-#     delete_book(book_to_delete)
-
-#     return redirect ('/books/list')
-
-@app.route('/books/delete/<id>', methods=['get'])
-def books_delete(id):
-    delete_book(int(id))
+@app.route('/books/delete/<index>', methods=['POST'])
+def books_delete(index):
+    delete_book(int(index))
     return redirect('/books')
 
-
-
-        
--- weekend/book_shop/models/book_list.py
+++ weekend/book_shop/models/book_list.py
@@ -1,8 +1,8 @@
 from models.book import *
 
-book1 = Book ("To Kill a Mockingbird", "Harper Lee", "Ethics")
-book2 = Book ("Bourne Identity", "Robert Ludlum", "Thriller")
-book3 = Book ("Catcher in the Rye", "J D Salinger", "Coming of Age ")
+book1 = Book ("To Kill a Mockingbird", "Harper Lee", "Ethics",True)
+book2 = Book ("Bourne Identity", "Robert Ludlum", "Thriller", False)
+book3 = Book ("Catcher in the Rye", "J D Salinger", "Coming of Age ", True)
 
 book_list = [book1, book2, book3]
 
@@ -10,5 +10,6 @@ book_list = [book1, book2, book3]
 def add_new_book(book):
     book_list.append(book)
 
-def delete_book(book_id):
-    book_list.pop(int(book_id))
+def delete_book(index):
+    book = book_list[index]
+    book_list.remove(book) 
